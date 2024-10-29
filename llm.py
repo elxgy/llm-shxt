b426bc28ba92aada36d5460d84acd7c43c565889
Elxgy <bernardozivianigasparino@gmail.com>
2024-10-29 16:10:50 -0300

    main code

---
+++ llm.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torchvision import datasets
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from torchvision.transforms import ToTensor
+
+
