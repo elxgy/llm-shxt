ca44770effab19a12da09cb70b355eadbeb7c93f
Elxgy <bernardozivianigasparino@gmail.com>
2024-10-29 17:46:22 -0300

    main code

--- llm.py
+++ llm.py
@@ -6,7 +6,16 @@ import torchvision
 import torchvision.transforms as transforms
 from torchvision import datasets
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
 from torchvision.transforms import ToTensor
+import tiktoken
 
 
+with open('data.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+enc = tiktoken.get_encoding('gpt2')
+
+data = torch.tensor(enc.encode(text), dtype=torch.long)
