bc3e06f49195c6c266efb4f37e36416ec2774050
wyv(notebook) <seireinomai@gmail.com>
2024-11-12 23:12:06 +0000

    deleted useless imports

--- llm.py
+++ llm.py
@@ -1,13 +1,7 @@
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import torchvision
-import torchvision.transforms as transforms
-from torchvision import datasets
-from torch.utils.data import DataLoader
-from torchvision.transforms import ToTensor
 import tiktoken
 
 block_size = 24  # max text chunk length
